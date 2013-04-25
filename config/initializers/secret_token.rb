e28709f1fb6a66edd765ffcb7922ca4e17f6947f
Chris Bolton <saiko-chriskun@Imperium.local>
2013-04-24 23:26:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Luxhaven::Application.config.secret_key_base = '78ce950430e699f21b9d01ea45e3b2fe29d684deec1f62a4bc776de8681cf88a7b21e465e4f3ab3d9ca297d7c76b74de88066899e746ef64b53891b7188ce46c'
