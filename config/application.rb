48f20c1238d338aadaab32418ee2152ac5d1f3a3
Chris Bolton <c@chrisbolton.me>
2013-04-26 18:46:44 -0700

    stylus production fix

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Luxhaven
     # config.i18n.default_locale = :de
     config.action_mailer.delivery_method   = :postmark
     config.action_mailer.postmark_settings = { api_key: '36ce3dca-315b-4369-8bfe-17b66285b0bc' }
-    Stylus.setup Sprockets, config.assets
+    Stylus.setup Sprockets, config.assets unless Rails.env == 'production'
   end
 end
