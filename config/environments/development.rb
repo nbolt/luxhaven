3ff8a99095906b8ff2c7071b69a4031a21de4605
Chris Bolton <c@chrisbolton.me>
2014-01-15 23:34:43 -0800

    testing heroku log fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,12 @@ Luxhaven::Application.configure do
 
   # Debug mode disables concatenation and preprocessing of assets.
   config.assets.debug = true
+
+  STDOUT.sync = true
+
+  logger = Logger.new(STDOUT)
+  logger.level = 0 # Must be numeric here - 0 :debug, 1 :info, 2 :warn, 3 :error, and 4 :fatal
+  # NOTE:   with 0 you're going to get all DB calls, etc.
+
+  Rails.logger = Rails.application.config.logger = logger
 end
